47e65ef4a70a4db082b3a848f614710bff63aa55
AshrafWzza <ashrafwzza2@gmail.com>
2022-12-05 02:43:31 +0200

    Localization based on system language

---
+++ lib/config/local/app_localizations.dart
@@ -0,0 +1,110 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+class AppLocalizations {
+  final Locale locale;
+
+  AppLocalizations(this.locale);
+
+  // Helper method to keep the code in the widgets concise
+  // Localizations are accessed using an InheritedWidget "of" syntax
+  static AppLocalizations? of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  // Static member to have a simple access to the delegate from the MaterialApp
+  static const LocalizationsDelegate<AppLocalizations> delegate =
+      _AppLocalizationsDelegate();
+
+  Map<String, String>? _localizedStrings;
+
+  Future<bool> load() async {
+    // Load the language JSON file from the "lang" folder
+    String jsonString =
+        await rootBundle.loadString('lang/${locale.languageCode}.json');
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+    _localizedStrings = jsonMap.map((key, value) {
+      return MapEntry(key, value.toString());
+    });
+
+    return true;
+  }
+
+  // This method will be called from every widget which needs a localized text
+  String? translate(String key) => _localizedStrings![key];
+}
+
+// LocalizationsDelegate is a factory for a set of localized resources
+// In this case, the localized strings will be gotten in an AppLocalizations object
+class _AppLocalizationsDelegate
+    extends LocalizationsDelegate<AppLocalizations> {
+  // This delegate instance will never change (it doesn't even have fields!)
+  // It can provide a constant constructor.
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    // Include all of your supported language codes here
+    return ['en', 'ar'].contains(locale.languageCode);
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    // AppLocalizations class is where the JSON loading actually runs
+
+    AppLocalizations localizations = AppLocalizations(locale);
+    await localizations.load();
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(_AppLocalizationsDelegate old) => false;
+}
+
+class AppLocalizationsSetup {
+  static const Iterable<Locale> supportedLocales = [
+    Locale('en'),
+    Locale('ar'),
+  ];
+
+  static const Iterable<LocalizationsDelegate<dynamic>> localizationsDelegates =
+      [
+    // A class which loads the translations from JSON files
+    AppLocalizations.delegate,
+    // Built-in localization of basic text for Material widgets
+    GlobalMaterialLocalizations.delegate,
+    // Built-in localization for text direction LTR/RTL
+    GlobalWidgetsLocalizations.delegate,
+    GlobalCupertinoLocalizations.delegate,
+    DefaultCupertinoLocalizations.delegate
+  ];
+
+  // Returns a locale which will be used by the app
+  static Locale? localeResolutionCallback(
+      Locale? locale, Iterable<Locale>? supportedLocales) {
+    // Locale?? Localizations.localeOf(context); // * Check for IOS
+    for (Locale supportedLocale in supportedLocales!) {
+      if (supportedLocale.languageCode == locale!.languageCode &&
+          supportedLocale.countryCode == locale.countryCode) {
+        return supportedLocale;
+      }
+    }
+    return supportedLocales.first;
+  }
+
+  static Locale? defaultLocale(Locale? locale) {
+    for (Locale supportedLocale in supportedLocales) {
+      if (supportedLocale.languageCode == locale?.languageCode &&
+          supportedLocale.countryCode == locale?.countryCode) {
+        return supportedLocale;
+      }
+    }
+    return supportedLocales.first;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'config/local/app_localizations.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,56 +14,48 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      // List all of the app's supported locales here
+      supportedLocales: AppLocalizationsSetup.supportedLocales,
+      // These delegates make sure that the localization data for the proper language is loaded
+      localizationsDelegates: AppLocalizationsSetup.localizationsDelegates,
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                AppLocalizations.of(context)!.translate('first_string')!,
+                style: const TextStyle(fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              Text(
+                AppLocalizations.of(context)!.translate('second_string')!,
+                style: const TextStyle(fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'This will not be translated.',
+                style: TextStyle(fontSize: 25),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
